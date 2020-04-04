30af3cda60929d059ab7a7db297dd7b89985a9a1
Benjamin Vaisvil <benjamini@gmail.com>
2020-04-03 23:01:01 -0500

    version 0.8.1

--- zenith.rb
+++ zenith.rb
@@ -2,12 +2,12 @@ class Zenith < Formula
     desc "sort of like top or htop but with histograms"
     homepage "https://github.com/bvaisvil/zenith"
     version "0.8.0"
-    url "https://github.com/bvaisvil/zenith/releases/download/0.8.0/zenith.MacOS.tgz"
-    sha256 "e21edfb055c6823c731229bc4b04886cc4e74109e2fcbb6611cbb6cd8a47efcc"
+    url "https://github.com/bvaisvil/zenith/releases/download/0.8.1/zenith.MacOS.tgz"
+    sha256 "ddb8fbeb436549a993eb2225829536dcb7d0336ea56af6bc4f9411e0c977e6ad"
   
     if OS.linux?
-      url "https://github.com/bvaisvil/zenith/releases/download/0.8.0/zenith.linux.tgz"
-      sha256 "f02e3ba2d71b1351a15214ec45b7c4249775ee44e9b39cb7e3b04ae59afcdb6b"
+      url "https://github.com/bvaisvil/zenith/releases/download/0.8.1/zenith.linux.tgz"
+      sha256 "a70cfe1230b9ef54208592dfbdfd6c6ff16e349edde839195421d501dda482ca"
     end
   
     def install
