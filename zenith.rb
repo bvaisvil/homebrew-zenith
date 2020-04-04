aeaaf54c0faa6fa58b1df628bb7801a0b3dd738c
Benjamin Vaisvil <benjamini@gmail.com>
2020-04-03 23:01:48 -0500

    missed a spot

--- zenith.rb
+++ zenith.rb
@@ -1,7 +1,7 @@
 class Zenith < Formula
     desc "sort of like top or htop but with histograms"
     homepage "https://github.com/bvaisvil/zenith"
-    version "0.8.0"
+    version "0.8.1"
     url "https://github.com/bvaisvil/zenith/releases/download/0.8.1/zenith.MacOS.tgz"
     sha256 "ddb8fbeb436549a993eb2225829536dcb7d0336ea56af6bc4f9411e0c977e6ad"
   
